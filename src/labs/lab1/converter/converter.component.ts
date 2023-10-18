7941d92e92a3762b8c2e6e52f14a82b4acdf8ec6
mmishv <pinina7773@gmail.com>
2023-10-18 14:41:49 +0300

    add report

--- src/labs/lab1/converter/converter.component.ts
+++ src/labs/lab1/converter/converter.component.ts
@@ -1,7 +1,7 @@
 import {
   Component, ElementRef, Input, OnInit, Renderer2
 } from '@angular/core';
-import {Color, ColorPickerControl} from "@iplab/ngx-color-picker";
+import {ColorPickerControl} from "@iplab/ngx-color-picker";
 import {distinctUntilChanged, Subscription} from "rxjs";
 
 interface RGB {
@@ -23,22 +23,14 @@ interface HSV {
   v: number;
 }
 
-interface HSL {
-  h: number;
-  s: number;
-  l: number;
-}
-
 @Component({
   selector: 'app-converter', templateUrl: './converter.component.html', styleUrls: ['./converter.component.css']
 })
 export class ConverterComponent implements OnInit {
   public colorControl = new ColorPickerControl();
-  private _color: Color = Color.from(`#FFFFFF`);
   protected RGB: RGB = {r: 0, g: 0, b: 0};
   protected CMYK: CMYK = {c: 0, m: 0, y: 0, k: 0};
   protected HSV: HSV = {h: 0, s: 0, v: 0};
-  protected HSL: HSL = {h: 0, s: 0, l: 0};
   private colorControlSubscription: Subscription | undefined;
 
   constructor(private renderer: Renderer2, private el: ElementRef) {
@@ -55,21 +47,20 @@ export class ConverterComponent implements OnInit {
   }
 
   onColorControlValueChange() {
-    const hsla = this.colorControl.value.getHsla();
-    this.HSL = {h: hsla.hue, s: hsla.saturation, l: hsla.lightness};
-    this.convertHSLtoHSV();
+    const hsv = this.colorControl.value.getHsva();
+    this.HSV = {h: Math.round(hsv.hue), s: Math.round(hsv.saturation), v: Math.round(hsv.value)};
     const rgba = this.colorControl.value.getRgba();
     this.RGB = {r: Math.round(rgba.red), g: Math.round(rgba.green), b: Math.round(rgba.blue)};
-    const cmyk = this.colorControl.value.getCmyk();
-    this.CMYK = {
-      c: Math.round(cmyk.cyan), m: Math.round(cmyk.magenta), y: Math.round(cmyk.yellow), k: Math.round(cmyk.black)
-    };
+    // const cmyk = this.colorControl.value.getCmyk();
+    // this.CMYK = {
+    //   c: Math.round(cmyk.cyan), m: Math.round(cmyk.magenta), y: Math.round(cmyk.yellow), k: Math.round(cmyk.black)
+    // };
+    this.rgbToCmyk();
   }
 
   @Input()
   public set color(color: string) {
     this.colorControl.setValueFrom(color);
-    this._color = this.colorControl.value;
   }
 
   onRGBChange() {
@@ -85,7 +76,9 @@ export class ConverterComponent implements OnInit {
     for (const prop of knownProperties) {
       this.CMYK[prop] = this.sanitizeValue(this.CMYK[prop], 100);
     }
-    this.color = `cmyk(${this.CMYK.c}, ${this.CMYK.m}, ${this.CMYK.y}, ${this.CMYK.k})`;
+    this.cmykToRgb();
+    this.color = `rgb(${this.RGB.r}, ${this.RGB.g}, ${this.RGB.b})`;
+    // this.color = `cmyk(${this.CMYK.c}, ${this.CMYK.m}, ${this.CMYK.y}, ${this.CMYK.k})`;
   }
 
   onHSVChange() {
@@ -94,8 +87,23 @@ export class ConverterComponent implements OnInit {
       this.HSV[prop] = this.sanitizeValue(this.HSV[prop], 100);
     }
     this.HSV.h = this.sanitizeValue(this.HSV.h, 360);
-    this.convertHSVtoHSL()
-    this.color = `hsl(${this.HSL.h}, ${this.HSL.s}, ${this.HSL.l})`;
+    this.hsvToRgb()
+    this.color = `rgb(${this.RGB.r}, ${this.RGB.g}, ${this.RGB.b})`;
+  }
+
+  RGBChange(prop: keyof RGB) {
+    this.RGB[prop] = this.sanitizeValue(this.RGB[prop], 255);
+    this.onRGBChange()
+  }
+
+  CMYKChange(prop: keyof CMYK) {
+    this.CMYK[prop] = this.sanitizeValue(this.CMYK[prop], 100);
+    this.onCMYKChange()
+  }
+
+  HSVChange(prop: keyof HSV) {
+    if (prop == 'h') this.HSV[prop] = this.sanitizeValue(this.HSV[prop], 360); else this.HSV[prop] = this.sanitizeValue(this.HSV[prop], 100);
+    this.onHSVChange();
   }
 
   sanitizeValue(value: number, maxValue: number): number {
@@ -104,19 +112,86 @@ export class ConverterComponent implements OnInit {
     return value;
   }
 
-  convertHSVtoHSL() {
-    const hsvToHsl = (h: number, s: number, v: number, l: number = v * (1 - (s / 2))) => [h, l === 0 || l === 1 ? 0 : (v - l) / Math.min(l, 1 - l), l];
-    let hsl = hsvToHsl(this.HSV.h, this.HSV.s / 100, this.HSV.v / 100);
-    this.HSL.h = hsl[0];
-    this.HSL.s = hsl[1] * 100;
-    this.HSL.l = hsl[2] * 100;
+  private hsvToRgb() {
+    let red = 1;
+    let green = 0;
+    let blue = 0;
+    const saturation = this.HSV.s / 100;
+    const brightness = this.HSV.v / 100;
+    const hex = this.HSV.h / 60;
+
+    const primary = Math.floor(hex);
+    const secondary = hex - primary;
+    const a = (1 - saturation) * brightness;
+    const b = (1 - (saturation * secondary)) * brightness;
+    const c = (1 - (saturation * (1 - secondary))) * brightness;
+
+    switch (primary) {
+      case 6:
+      case 0:
+        red = brightness;
+        green = c;
+        blue = a;
+        break;
+      case 1:
+        red = b;
+        green = brightness;
+        blue = a;
+        break;
+      case 2:
+        red = a;
+        green = brightness;
+        blue = c;
+        break;
+      case 3:
+        red = a;
+        green = b;
+        blue = brightness;
+        break;
+      case 4:
+        red = c;
+        green = a;
+        blue = brightness;
+        break;
+      case 5:
+        red = brightness;
+        green = a;
+        blue = b;
+        break;
+    }
+    this.RGB.r = red * 255;
+    this.RGB.g = green * 255;
+    this.RGB.b = blue * 255;
   }
 
-  convertHSLtoHSV() {
-    const hslToHsv = (h: number, s: number, l: number, v: number = l + s * Math.min(l, 1 - l)) => [h, v === 0 ? 0 : 2 * (1 - (l / v)), v];
-    let hsv = hslToHsv(this.HSL.h, this.HSL.s / 100, this.HSL.l / 100);
-    this.HSV.h = Math.round(hsv[0]);
-    this.HSV.s = Math.round(hsv[1] * 100);
-    this.HSV.v = Math.round(hsv[2] * 100);
+  private cmykToRgb() {
+    const black = this.CMYK.k / 100;
+    const cyan = this.CMYK.c / 100;
+    const magenta = this.CMYK.m / 100;
+    const yellow = this.CMYK.y / 100;
+    let red = (1 - cyan) * (1 - black);
+    let green = (1 - magenta) * (1 - black);
+    let blue = (1 - yellow) * (1 - black);
+    this.RGB.r = Math.round(red * 255);
+    this.RGB.g = Math.round(green * 255);
+    this.RGB.b = Math.round(blue * 255);
+  }
+
+  private rgbToCmyk() {
+    const red = this.RGB.r / 255;
+    const green = this.RGB.g / 255;
+    const blue = this.RGB.b / 255;
+    let black = 1 - Math.max(red, green, blue);
+    if (black === 1) {
+      this.CMYK.k = 0;
+      this.CMYK.m = 0;
+      this.CMYK.y = 0;
+      this.CMYK.k = 100;
+      return;
+    }
+    this.CMYK.c = Math.round((1 - red - black)/(1-black) * 100);
+    this.CMYK.m = Math.round((1 - green - black)/(1-black) * 100);
+    this.CMYK.y = Math.round((1 - blue - black)/(1-black) * 100);
+    this.CMYK.k = Math.round(black * 100);
   }
 }
